91a1c794988c244d233e90f221f5ad49addc7c70
Henry <me@hnry.us>
2016-01-31 21:36:39 -0500

    Task successfully getting data from the API

--- app/src/main/java/us/hnry/fancy/data/FetchStockTask.java
+++ app/src/main/java/us/hnry/fancy/data/FetchStockTask.java
@@ -6,12 +6,15 @@ import android.os.AsyncTask;
 import android.util.Log;
 
 import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
 
 /**
  * Created by Henry on 1/31/2016.
+ *
  */
 public class FetchStockTask extends AsyncTask<Void, Void, Void> {
 
@@ -42,8 +45,47 @@ public class FetchStockTask extends AsyncTask<Void, Void, Void> {
 
             URL url = new URL(builtUri.toString());
             Log.v(LOG_TAG, url.toString());
-        } catch (MalformedURLException e) {
+
+            //Create the request to the API
+            urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setRequestMethod("GET");
+            urlConnection.connect();
+
+            // Read the input stream into a string
+            InputStream inputStream = urlConnection.getInputStream();
+            StringBuffer buffer = new StringBuffer();
+            if(inputStream == null){
+                Log.v(LOG_TAG, "Empty input stream");
+                return null;
+            }
+            reader = new BufferedReader(new InputStreamReader(inputStream));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                // Since it's JSON, adding a newline isn't necessary (it won't affect parsing)
+                // But it does make debugging a *lot* easier if you print out the completed
+                // buffer for debugging.
+                buffer.append(line + "\n");
+            }
+
+            if (buffer.length() == 0) {
+                // Stream was empty.  No point in parsing.
+                return null;
+            }
+            yahooJSONString = buffer.toString();
+
+        } catch (IOException e) {
             e.printStackTrace();
+        } finally {
+            if (urlConnection != null) {
+                urlConnection.disconnect();
+            }
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (final IOException e) {
+                    Log.e(LOG_TAG, "Error closing stream", e);
+                }
+            }
         }
 
         return null;
