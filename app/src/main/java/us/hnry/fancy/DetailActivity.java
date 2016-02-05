ecbadb8af88be5794ded9ce251e7a91d979dd9c8
Henry <me@hnry.us>
2016-02-05 15:57:16 -0500

    Detail view now in a fragment

--- app/src/main/java/us/hnry/fancy/DetailActivity.java
+++ app/src/main/java/us/hnry/fancy/DetailActivity.java
@@ -1,14 +1,18 @@
 package us.hnry.fancy;
 
+import android.app.Fragment;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -27,35 +31,7 @@ import us.hnry.fancy.utils.Utility;
 
 public class DetailActivity extends AppCompatActivity {
 
-    Map<String, String> map;
-    private ArrayList<String> keys;
-    private Method[] methods;
-    private ListView mDetailListView;
-
-    private Stock fromIntent;
-    private Set<String> mSetOfStocks;
-    private boolean isTracked;
-
-    private DetailAdapter consumeParcelableStockFromIntent(Stock stock)
-            throws InvocationTargetException, IllegalAccessException {
-
-        keys = new ArrayList<>();
-        methods = stock.getClass().getMethods();
-        map = new HashMap<>();
-
-        for (Method m : methods) {
-            if (m.getName().startsWith("get")) {
-                String value = String.valueOf(m.invoke(stock));
-                String name = m.getName().substring(3);
-                if (!name.equals("Class")) {
-                    map.put(name, value);
-                    keys.add(name);
-                }
-            }
-        }
-
-        return new DetailAdapter(this, keys, map);
-    }
+    static final String FRAGMENT_TAG_DETAIL = "fragment_detail";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -63,81 +39,133 @@ public class DetailActivity extends AppCompatActivity {
         setContentView(R.layout.activity_detail);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        isTracked = false;
-        SharedPreferences preferences = getSharedPreferences(Utility.PERSISTENT, Context.MODE_PRIVATE);
-        final SharedPreferences.Editor editor = preferences.edit();
-
-        final FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                /*Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();*/
-                if (isTracked) {
-                    mSetOfStocks.remove(fromIntent.getSymbol());
-                    fab.setImageResource(R.drawable.ic_favorite_white_24dp);
-                    isTracked = false;
-                    Snackbar.make(view, "Removed from Favorites", Snackbar.LENGTH_SHORT).show();
-                } else {
-                    mSetOfStocks.add(fromIntent.getSymbol());
-                    fab.setImageResource(R.drawable.ic_check_circle_white_24dp);
-                    isTracked = true;
-                    Snackbar.make(view, "Added to Favorites", Snackbar.LENGTH_SHORT).show();
-                }
-                editor.clear();
-                editor.putStringSet(Utility.PERSISTENT_SYMBOLS_SET, mSetOfStocks);
-                editor.apply();
-            }
-        });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        final ProgressDialog progressDialog = new ProgressDialog(this);
-        progressDialog.setMessage("Fetching data");
-        progressDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
-        progressDialog.setCancelable(false);
-        progressDialog.setIndeterminate(true);
-        progressDialog.show();
-
-        mDetailListView = (ListView) findViewById(R.id.content_detail_list_view);
-        fromIntent = getIntent().getParcelableExtra(Utility.STOCK_INTENT);
-        if (fromIntent != null) {
-            setTitle(fromIntent.getName());
-            mSetOfStocks = preferences.getStringSet(Utility.PERSISTENT_SYMBOLS_SET,
-                    new HashSet<>(Arrays.asList(Utility.DEFAULT_SYMBOLS)));
-            if (mSetOfStocks.contains(fromIntent.getSymbol())) {
-                fab.setImageResource(R.drawable.ic_check_circle_white_24dp);
-                isTracked = true;
-            }
-            new Thread(){
-                @Override
-                public void run() {
-                    try {
-                        final DetailAdapter detailAdapter = consumeParcelableStockFromIntent(fromIntent);
-                        runOnUiThread(new Runnable() {
-                            @Override
-                            public void run() {
-                                mDetailListView.setAdapter(detailAdapter);
-                                progressDialog.dismiss();
-                            }
-                        });
-                    } catch (InvocationTargetException | IllegalAccessException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }.start();
-
-        } else {
-            Toast.makeText(this,
-                    "Selection returned invalid results from the server.",
-                    Toast.LENGTH_SHORT).show();
-            finish();
-        }
+        getFragmentManager()
+                .beginTransaction()
+                .add(R.id.detail_container,
+                        new DetailFragment(),
+                        FRAGMENT_TAG_DETAIL)
+                .commit();
     }
 
     @Override
     public void onBackPressed() {
         super.onBackPressed();
         finish();
+    }
+
+    public static class DetailFragment extends Fragment {
+        Map<String, String> map;
+        private ArrayList<String> keys;
+        private Method[] methods;
+        private ListView mDetailListView;
+        private FloatingActionButton fab;
+
+        private Stock fromIntent;
+        private Set<String> mSetOfStocks;
+        private boolean isTracked;
+
+        private ProgressDialog mProgressDialog;
+
+        @Override
+        public void onActivityCreated(Bundle savedInstanceState) {
+            super.onActivityCreated(savedInstanceState);
+
+            mProgressDialog = new ProgressDialog(getActivity());
+            mProgressDialog.setMessage("Fetching data");
+            mProgressDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+            mProgressDialog.setCancelable(false);
+            mProgressDialog.setIndeterminate(true);
+            mProgressDialog.show();
+        }
+
+        @Nullable
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+            View layout = inflater.inflate(R.layout.fragment_detail, container, false);
+            isTracked = false;
+            SharedPreferences preferences = getActivity().getSharedPreferences(Utility.PERSISTENT, Context.MODE_PRIVATE);
+            final SharedPreferences.Editor editor = preferences.edit();
+
+            fab = (FloatingActionButton) getActivity().findViewById(R.id.fab);
+            fab.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                /*Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();*/
+                    if (isTracked) {
+                        mSetOfStocks.remove(fromIntent.getSymbol());
+                        fab.setImageResource(R.drawable.ic_favorite_white_24dp);
+                        isTracked = false;
+                        Snackbar.make(view, "Removed from Favorites", Snackbar.LENGTH_SHORT).show();
+                    } else {
+                        mSetOfStocks.add(fromIntent.getSymbol());
+                        fab.setImageResource(R.drawable.ic_check_circle_white_24dp);
+                        isTracked = true;
+                        Snackbar.make(view, "Added to Favorites", Snackbar.LENGTH_SHORT).show();
+                    }
+                    editor.clear();
+                    editor.putStringSet(Utility.PERSISTENT_SYMBOLS_SET, mSetOfStocks);
+                    editor.apply();
+                }
+            });
+
+            mDetailListView = (ListView) layout.findViewById(R.id.content_detail_list_view);
+            fromIntent = getActivity().getIntent().getParcelableExtra(Utility.STOCK_INTENT);
+            if (fromIntent != null) {
+                getActivity().setTitle(fromIntent.getName());
+                mSetOfStocks = preferences.getStringSet(Utility.PERSISTENT_SYMBOLS_SET,
+                        new HashSet<>(Arrays.asList(Utility.DEFAULT_SYMBOLS)));
+                if (mSetOfStocks.contains(fromIntent.getSymbol())) {
+                    fab.setImageResource(R.drawable.ic_check_circle_white_24dp);
+                    isTracked = true;
+                }
+                new Thread() {
+                    @Override
+                    public void run() {
+                        try {
+                            final DetailAdapter detailAdapter = consumeParcelableStockFromIntent(fromIntent);
+                            getActivity().runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    mDetailListView.setAdapter(detailAdapter);
+                                    mProgressDialog.dismiss();
+                                }
+                            });
+                        } catch (InvocationTargetException | IllegalAccessException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }.start();
+
+            } else {
+                Toast.makeText(getActivity(),
+                        "Selection returned invalid results from the server.",
+                        Toast.LENGTH_SHORT).show();
+                getActivity().finish();
+            }
+            return layout;
+        }
+
+        private DetailAdapter consumeParcelableStockFromIntent(Stock stock)
+                throws InvocationTargetException, IllegalAccessException {
+
+            keys = new ArrayList<>();
+            methods = stock.getClass().getMethods();
+            map = new HashMap<>();
+
+            for (Method m : methods) {
+                if (m.getName().startsWith("get")) {
+                    String value = String.valueOf(m.invoke(stock));
+                    String name = m.getName().substring(3);
+                    if (!name.equals("Class")) {
+                        map.put(name, value);
+                        keys.add(name);
+                    }
+                }
+            }
+            return new DetailAdapter(getActivity(), keys, map);
+        }
     }
 }
